bfd2acb8c245e1a6cb6452e73ddba374d2f5d0f5
iamdude11 <yedhuku2003@gmail.com>
2024-08-31 17:51:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '15529802'))
 API_HASH = environ.get('API_HASH', '92bcb6aa798a6f1feadbc917fccb54d3')
 OWNER_ID = environ.get('OWNER_ID', '821215952')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5277636826:AAHvAKmE-5XuzNOINzZPkAmLGdXQIS-mDRw")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
